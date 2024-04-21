2e5c0725389a36190c7614f72f92ba5c1b00ebc3
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-04-21 13:25:32 +0530

    Commits

---
+++ src/StarPattern3.java
@@ -0,0 +1,17 @@
+public class StarPattern3 {
+    public static void main(String[] args) {
+        int rows = 5; // You can change this value to increase or decrease the size of the triangle
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= (rows - i) * 2; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
