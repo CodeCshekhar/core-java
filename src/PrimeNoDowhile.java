48eac85ab17afc4c95d8a6b5e7f40586b29e7282
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-04-21 13:54:56 +0530

    Commits

---
+++ src/PrimeNoDowhile.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeNoDowhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        int i = 2;
+        boolean isPrime = true;
+        do {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+            i++;
+        } while (i <= num / 2);
+
+        if (isPrime && num > 1) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
---
+++ src/PrimeWhileLoop.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeWhileLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        int i = 2;
+        boolean isPrime = true;
+        while (i <= num / 2) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+            i++;
+        }
+
+        if (isPrime && num > 1) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
