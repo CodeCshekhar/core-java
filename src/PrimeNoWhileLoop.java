acf77d0bbf2d04f65fa5d6028346fd10af5a1689
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-04-21 13:56:06 +0530

    Commits

--- src/PrimeNoWhileLoop.java
+++ src/PrimeNoWhileLoop.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PrimeWhileLoop {
+public class PrimeNoWhileLoop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
