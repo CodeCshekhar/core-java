670c8f63d34fe0f1eed64e31ca1bf63579e91d1a
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-05-16 18:59:56 +0530

    Commits

--- src/Collections/HashSet/GetNumberOfElements.java
+++ src/Collections/HashSet/GetNumberOfElements.java
@@ -6,17 +6,17 @@ public class GetNumberOfElements {
     public static void main(String[] args) {
 
              // Create a empty hash set
-        HashSet<String> h_set = new HashSet<String>();
+        HashSet<String> hashset = new HashSet<String>();
             // use add() method to add values in the hash set
-                h_set.add("Red");
-                h_set.add("Green");
-                h_set.add("Black");
-                h_set.add("White");
-                h_set.add("Pink");
-                h_set.add("Yellow");
+                hashset.add("Red");
+                hashset.add("Green");
+                hashset.add("Black");
+                hashset.add("White");
+                hashset.add("Pink");
+                hashset.add("Yellow");
 
-            System.out.println("Original Hash Set: " + h_set);
-            System.out.println("Size of the Hash Set: " + h_set.size());
+            System.out.println("Original Hash Set: " + hashset);
+            System.out.println("Size of the Hash Set: " + hashset.size());
         }
     }
 
