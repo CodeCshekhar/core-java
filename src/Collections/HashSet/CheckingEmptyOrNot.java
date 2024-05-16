850dade3b56b3ddd2ba54439fa9dca71b890123b
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-05-16 18:58:44 +0530

    Commits

--- src/Collections/HashSet/CheckingEmptyOrNot.java
+++ src/Collections/HashSet/CheckingEmptyOrNot.java
@@ -6,21 +6,21 @@ public class CheckingEmptyOrNot {
     public static void main(String[] args) {
 
             // Create a empty hash set
-            HashSet<String> h_set = new HashSet();
+            HashSet<String> hashset = new HashSet();
             // use add() method to add values in the hash set
-                h_set.add("Red");
-                h_set.add("Green");
-                h_set.add("Black");
-                h_set.add("White");
-                h_set.add("Pink");
-                h_set.add("Yellow");
+                hashset.add("Red");
+                hashset.add("Green");
+                hashset.add("Black");
+                hashset.add("White");
+                hashset.add("Pink");
+                hashset.add("Yellow");
 
-                System.out.println("Original Hash Set: " + h_set);
-                System.out.println("Checking the above array list is empty or not! "+h_set.isEmpty());
+                System.out.println("Original Hash Set: " + hashset);
+                System.out.println("Checking the above array list is empty or not! "+hashset.isEmpty());
                 System.out.println("Remove all the elements from a Hash Set: ");
 
-                 h_set.removeAll(h_set);
-                System.out.println("Hash Set after removing all the elements "+h_set);
+                 hashset.removeAll(hashset);
+                System.out.println("Hash Set after removing all the elements "+hashset);
         }
     }
 
