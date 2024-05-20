deab44d9dfedff520b9d1e49f178cf7cf07ca39d
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-05-20 14:13:53 +0530

    Commits

--- src/Collections/HashMap/MakeCopy.java
+++ src/Collections/HashMap/MakeCopy.java
@@ -5,22 +5,22 @@ import java.util.HashMap;
 public class MakeCopy {
     public static void main(String args[]) {
             // create two hash maps
-            HashMap <Integer,String> hashmap1 = new HashMap <Integer,String> ();
-            HashMap <Integer,String> hashmap2 = new HashMap<Integer,String>();
+            HashMap <Integer,String> hashmap = new HashMap <Integer,String> ();
+            HashMap <Integer,String> hashmap1 = new HashMap<Integer,String>();
 
             // populate hash maps
-            hashmap1.put(1, "Red");
-            hashmap1.put(2, "Green");
-            hashmap1.put(3, "Black");
-            System.out.println("\nValues in first map: " + hashmap1);
-            hashmap2.put(4, "White");
-            hashmap2.put(5, "Blue");
-            hashmap2.put(6, "Orange");
-            System.out.println("\nValues in second map: " + hashmap2);
+            hashmap.put(1, "Red");
+            hashmap.put(2, "Green");
+            hashmap.put(3, "Black");
+            System.out.println("\nValues in first map: " + hashmap);
+            hashmap1.put(4, "White");
+            hashmap1.put(5, "Blue");
+            hashmap1.put(6, "Orange");
+            System.out.println("\nValues in second map: " + hashmap1);
 
             // put all values in secondmap
-            hashmap2.putAll(hashmap1);
-            System.out.println("\nNow values in second map: " + hashmap2);
+            hashmap1.putAll(hashmap);
+            System.out.println("\nNow values in second map: " + hashmap1);
         }
     }
 
