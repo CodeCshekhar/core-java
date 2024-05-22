d92697391ada163101ddd50fcdeda8673204a61a
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-05-22 13:02:54 +0530

    Commits

---
+++ src/Collections/ArrayList/TrimArray.java
@@ -0,0 +1,20 @@
+package Collections.ArrayList;
+
+import java.util.ArrayList;
+
+public class TrimArray {
+    public static void main(String[] args) {
+            ArrayList<String> a1= new ArrayList<String>();
+            a1.add("Red");
+            a1.add("Green");
+            a1.add("Black");
+            a1.add("White");
+            a1.add("Pink");
+            System.out.println("Original array list: " + a1);
+            System.out.println("Let trim to size the above array: ");
+            a1.trimToSize();
+            System.out.println(a1);
+        }
+    }
+
+
