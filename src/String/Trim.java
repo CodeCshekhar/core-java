48577bbcdbd55cabf0913de67fa14c6a1ec78308
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-05-29 13:07:50 +0530

    Commits

---
+++ src/String/Trim.java
@@ -0,0 +1,10 @@
+package String;
+
+public class Trim {
+    public static void main(String[] args) {
+            String s = " Sachin ";
+            System.out.println(s.trim()); // Output: Sachin
+        }
+    }
+
+
