8dc7f7b135be930c3b3d34fc950e0fa23e8f8cc1
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-06-04 15:45:33 +0530

    Commits

---
+++ src/OOPS/Inheritance/Vehicle/TestVehicle.java
@@ -0,0 +1,11 @@
+package OOPS.Inheritance.Vehicle;
+
+public class TestVehicle {
+
+    public static void main(String[] args) {
+        Bike Bk = new Bike();
+    System.out.println(Bk.fuelType + "\n" + Bk.transmission);
+    System.out.println(Bk.maxSpeed + "\n" + Bk.bikeType);
+    }
+
+}
