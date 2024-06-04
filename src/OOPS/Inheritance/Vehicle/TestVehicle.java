8674905975c55c1ee84ff6da6ea4e58a36225323
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-06-04 15:56:16 +0530

    Commits

--- src/OOPS/Inheritance/Vehicle/TestVehicle.java
+++ src/OOPS/Inheritance/Vehicle/TestVehicle.java
@@ -3,9 +3,25 @@ package OOPS.Inheritance.Vehicle;
 public class TestVehicle {
 
     public static void main(String[] args) {
+        Vehicle Vc = new Vehicle();
+        System.out.println("Bike has =" + "\n" + Vc.fuelType + "\n" + Vc.transmission);
+
+        System.out.println("------------------");
+
         Bike Bk = new Bike();
-    System.out.println(Bk.fuelType + "\n" + Bk.transmission);
+        System.out.println("Bike has =" + "\n" + Bk.bikeType + "\n" + Bk.maxSpeed);
+
+        System.out.println("------------------");
+
+        Car Cr = new Car();
+        System.out.println("Car has =" + "\n" + Cr.fuel + "\n" + Cr.seats);
+
+        System.out.println("------------------");
+
+        Truck Tr = new Truck();
+        System.out.println("Truck has =" + "\n" + Tr.LoadCapacity + "\n" + Tr.wheels);
 
     }
+
 
 }
