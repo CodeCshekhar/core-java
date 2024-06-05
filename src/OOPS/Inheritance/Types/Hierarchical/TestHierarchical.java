c3455d148dcd189133368cd1f828784def083405
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-06-05 18:25:41 +0530

    Commits

--- src/OOPS/Inheritance/Types/Hierarchical/TestHierarchical.java
+++ src/OOPS/Inheritance/Types/Hierarchical/TestHierarchical.java
@@ -2,9 +2,9 @@ package OOPS.Inheritance.Types.Hierarchical;
 
 public class TestHierarchical {
     public static void main(String[] args) {
-        B s2 = new B();
-        C s3 = new C();
-        D s4 = new D();
-        System.out.println(s2.s1+s2.s2+"\n"+s3.s1+s3.s3+"\n"+s4.s1+s4.s4);
+        B B1 = new B();
+        C C2 = new C();
+        D D3 = new D();
+        System.out.println(B1.s1+B1.s2+"\n"+C2.s1+C2.s3+"\n"+D3.s1+D3.s4);
     }
 }
