7013245930b1af18f7042abaeb945409d3cfcbaf
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-06-20 11:19:46 +0530

    commits

--- src/exceptionHandling/customException/bankAccount.java
+++ src/exceptionHandling/customException/bankAccount.java
@@ -1,24 +1,26 @@
 package exceptionHandling.customException;
 
 // Define a custom exception by extending the Exception class
-class CustomException extends Exception {
+class customException extends Exception {
 
-    public CustomException(String message) {
+    public customException(String message) {
+
         super(message);
     }
 }
 
-public class BankAccount {
+public class bankAccount {
     private double balance;
 
-    public BankAccount(double initialBalance) {
+    public bankAccount(double initialBalance) {
+
         this.balance = initialBalance;
     }
 
 
-    public void withdraw(double amount) throws CustomException {
+    public void withdraw(double amount) throws customException {
         if (amount > balance) {
-            throw new CustomException("Insufficient funds for withdrawal");
+            throw new customException("Insufficient funds for withdrawal");
         }
         balance -= amount;
         System.out.println("Withdrawal successful. Remaining balance: " + balance);
@@ -26,11 +28,11 @@ public class BankAccount {
 
 
     public static void main(String[] args) {
-        BankAccount account = new BankAccount(1000.00);
+        bankAccount account = new bankAccount(1000.00);
 
         try {
-            account.withdraw(1500.00); // This will throw the CustomException
-        } catch (CustomException e) {
+            account.withdraw(1500.00); // This will throw the customException
+        } catch (customException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
