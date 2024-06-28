5538cdc6d1f2270bd12ee25b4ba633c6e3798462
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-06-28 09:20:46 +0530

    commits

---
+++ src/exceptionHandling/customException/catchingCustomException.java
@@ -0,0 +1,19 @@
+package exceptionHandling.customException;
+
+class CustomException extends Exception {
+        public CustomException(String message) {
+            super(message);
+        }
+    }
+
+    public class catchingCustomException {
+        public static void main(String[] args) {
+            try {
+                throw new CustomException("This is a custom exception");
+            } catch (CustomException e) {
+                System.out.println("Custom Exception caught: " + e.getMessage());
+            }
+        }
+    }
+
+
