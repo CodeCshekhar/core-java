95b056b54a61ba292f7b849498b4b2918f7352e6
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-07-06 19:39:24 +0530

    commits

--- src/fileHandling/fileReader.java
+++ src/fileHandling/fileReader.java
@@ -4,11 +4,31 @@ import java.io.FileReader;
 
 public class fileReader {
     public static void main(String[] args) throws Exception {
-        FileReader file = new FileReader("E:\\file handling.txt");
+
+        //Reading txt file
+        FileReader file = new FileReader("E:\\Any file name.txt");
 
        int i ;
         while ((i = file.read())!=-1){
             System.out.println((char) i);
+
+
+        //Reading json file
+        FileReader file1 = new FileReader("E:\\Any file name.json");
+
+            int o ;
+            while ((o = file1.read())!=-1){
+                System.out.println((char) o);
+
+        //Reading xml file
+        FileReader file2 = new FileReader("E:\\Any file name.xml");
+
+                int p ;
+                while ((p = file2.read())!=-1){
+                    System.out.println((char) p);
         }
     }
+}
+}
+
 }
