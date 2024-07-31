59aaa89be149af1d8846a3a5bf0e677dad4b4363
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-07-31 19:08:18 +0530

    commits

---
+++ src/breakContinue/DoWhileContinue.java
@@ -0,0 +1,14 @@
+package breakContinue;
+
+public class DoWhileContinue {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            i++;
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        } while (i < 10);
+    }
+}
