4bf494cd671c5c83bddc67af3d57842b2cb83ab3
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-04 13:32:51 +0530

    commits

---
+++ src/lambdaExpression/Sorting.java
@@ -0,0 +1,16 @@
+package lambdaExpression;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Sorting {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Aarav", "Bhavna", "Chirag");
+        List<Integer> nameLengths = names.stream()
+                .map(name -> name.length())
+                .collect(Collectors.toList());
+        System.out.println(nameLengths); // Output: [5, 6, 6]
+
+    }
+}
