65eecad2ed8223417bb2392097068a71802fc349
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-04 13:33:18 +0530

    commits

---
+++ src/breakContinue/WhileLoopContinue.java
@@ -0,0 +1,14 @@
+package breakContinue;
+
+public class WhileLoopContinue {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 10) {
+            count++;
+            if (count % 3 == 0) {
+                continue;
+            }
+            System.out.println(count);
+        }
+    }
+}
