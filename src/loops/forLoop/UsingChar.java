57bbf76c32bb63dd0b854143f9c942a6c1a54b5d
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-07 21:24:49 +0530

    commits

---
+++ src/loops/forLoop/UsingChar.java
@@ -0,0 +1,9 @@
+package loops.forLoop;
+
+public class UsingChar {
+    public static void main(String[] args) {
+        for (char c = 'a'; c <= 'e'; c++) {
+            System.out.println("c is: " + c);
+        }
+    }
+}
