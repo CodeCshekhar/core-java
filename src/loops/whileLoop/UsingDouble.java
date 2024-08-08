4f5e492cce7261359074b6d86541a804442cc5d5
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-08 22:02:00 +0530

    commits

---
+++ src/loops/whileLoop/UsingDouble.java
@@ -0,0 +1,11 @@
+package loops.whileLoop;
+
+public class UsingDouble {
+    public static void main(String[] args) {
+        double d = 0.0;
+        while (d < 2.5) {
+            System.out.println("d is: " + d);
+            d += 0.5;
+        }
+    }
+}
