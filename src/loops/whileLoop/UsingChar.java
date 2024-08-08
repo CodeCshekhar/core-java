af94eb02017195446bd33d5ece37e03ee4ec4746
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-08 22:01:34 +0530

    commits

---
+++ src/loops/whileLoop/UsingChar.java
@@ -0,0 +1,11 @@
+package loops.whileLoop;
+
+public class UsingChar {
+    public static void main(String[] args) {
+        char c = 'a';
+        while (c <= 'e') {
+            System.out.println("c is: " + c);
+            c++;
+        }
+    }
+}
