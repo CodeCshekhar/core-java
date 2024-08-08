17fed69e86bffd35d599d100113c60dc42f5c79c
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-08 22:01:52 +0530

    commits

---
+++ src/loops/forLoop/UsingDouble.java
@@ -0,0 +1,9 @@
+package loops.forLoop;
+
+public class UsingDouble {
+    public static void main(String[] args) {
+        for (double d = 0.0; d < 2.5; d += 0.5) {
+            System.out.println("d is: " + d);
+        }
+    }
+}
