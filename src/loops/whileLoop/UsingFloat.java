1179db652ea79a66294c4070f2261dcfb014a6ed
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-09 12:42:17 +0530

    commits

---
+++ src/loops/whileLoop/UsingFloat.java
@@ -0,0 +1,11 @@
+package loops.whileLoop;
+
+public class UsingFloat {
+    public static void main(String[] args) {
+        float f = 0.0f;
+        while (f < 2.5f) {
+            System.out.println("f is: " + f);
+            f += 0.5f;
+        }
+    }
+}
