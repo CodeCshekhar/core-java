41180157fc74111b77811955aef4686f6c5c27e9
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-09 12:42:08 +0530

    commits

---
+++ src/loops/forLoop/UsingFloat.java
@@ -0,0 +1,9 @@
+package loops.forLoop;
+
+public class UsingFloat {
+    public static void main(String[] args) {
+        for (float f = 0.0f; f < 2.5f; f += 0.5f) {
+            System.out.println("f is: " + f);
+        }
+    }
+}
