24582b572c475696512daee1ac33bed3b11c7eec
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-09 12:42:27 +0530

    commits

---
+++ src/loops/doWhile/UsingInteger.java
@@ -0,0 +1,12 @@
+package loops.doWhile;
+
+public class UsingInteger {
+    public static void main(String[] args) {
+            int i = 0;
+            do {
+                System.out.println("i is: " + i);
+                i++;
+            } while (i < 5);
+        }
+    }
+
