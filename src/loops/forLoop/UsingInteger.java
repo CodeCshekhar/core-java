30666f204e48c69110113093d9303511be31735c
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-10 11:31:29 +0530

    commits

---
+++ src/loops/forLoop/UsingInteger.java
@@ -0,0 +1,9 @@
+package loops.forLoop;
+
+public class UsingInteger {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i is: " + i);
+        }
+    }
+}
