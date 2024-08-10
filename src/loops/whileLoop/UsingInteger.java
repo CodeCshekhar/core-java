99fbf4b2f0edb268636816ba709495592d9bf39e
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-10 11:31:44 +0530

    commits

---
+++ src/loops/whileLoop/UsingInteger.java
@@ -0,0 +1,11 @@
+package loops.whileLoop;
+
+public class UsingInteger {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println("i is: " + i);
+            i++;
+        }
+    }
+}
