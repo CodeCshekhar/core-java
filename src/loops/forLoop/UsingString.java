40884bbe18cfe296909dca443ac849dfbfa1fd0c
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-12 16:14:54 +0530

    commits

---
+++ src/loops/forLoop/UsingString.java
@@ -0,0 +1,10 @@
+package loops.forLoop;
+
+public class UsingString {
+    public static void main(String[] args) {
+        String[] strings = {"one", "two", "three", "four", "five"};
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println("str is: " + strings[i]);
+        }
+}
+}
