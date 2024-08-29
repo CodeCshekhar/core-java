0fa5dce66d854476d3e6e57d4787aab5e268486c
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-29 15:53:03 +0530

    commits

---
+++ src/string/EndsWith.java
@@ -0,0 +1,10 @@
+package string;
+
+public class EndsWith {
+    static String str = "Hello.txt";
+    static boolean result = str.endsWith(".txt"); // Returns true
+
+    public static void main(String[] args) {
+        System.out.println(result);
+    }
+}
