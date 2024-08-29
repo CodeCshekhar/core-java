9bb4fbf033fb68b9f50eb49c685e3b4d6642662c
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-29 15:52:50 +0530

    commits

---
+++ src/string/Contains.java
@@ -0,0 +1,10 @@
+package string;
+
+public class Contains {
+    static String str = "Hello World";
+    static boolean result = str.contains("World"); // Returns true
+
+    public static void main(String[] args) {
+        System.out.println(result);
+    }
+}
