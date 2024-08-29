c71bea95e892de2591d23c5f6e240f704f25d85a
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-29 15:52:09 +0530

    commits

---
+++ src/string/CompareTo.java
@@ -0,0 +1,11 @@
+package string;
+
+public class CompareTo {
+    static String str1 = "apple";
+    static String str2 = "banana";
+    static int result = str1.compareTo(str2); // Returns a negative value
+
+    public static void main(String[] args) {
+        System.out.println(result);
+    }
+}
