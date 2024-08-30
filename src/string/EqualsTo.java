1459de1658ad052b4f0b46f182f6cb38540f6484
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-30 19:31:07 +0530

    commits

---
+++ src/string/EqualsTo.java
@@ -0,0 +1,11 @@
+package string;
+
+public class EqualsTo {
+    static String str1 = "Hello";
+    static String str2 = "Hello";
+    static boolean result = str1.equals(str2); // Returns true
+
+    public static void main(String[] args) {
+        System.out.println(result);
+    }
+}
