4f21abfbf367207802bfadb8a33c0fec713d28f2
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-30 19:31:16 +0530

    commits

---
+++ src/string/Format.java
@@ -0,0 +1,10 @@
+package string;
+
+public class Format {
+    static String formatted = String.format("My name is %s and I'm %d years old", "John", 25);
+    // Returns "My name is John and I'm 25 years old"
+
+public static void main(String[] args) {
+    System.out.println(formatted);
+}
+}
