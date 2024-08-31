917efbd446fcd95d2dd03a99cf5438da95315f22
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-31 15:45:27 +0530

    commits

---
+++ src/string/IndexOf.java
@@ -0,0 +1,10 @@
+package string;
+
+public class IndexOf {
+    static String str = "Hello World";
+    static int index = str.indexOf("World"); // Returns 6
+
+    public static void main(String[] args) {
+        System.out.println(index);
+    }
+}
