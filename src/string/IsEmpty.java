c1725d675e26db670367ff082a3647a73d30e3a0
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-31 15:45:36 +0530

    commits

---
+++ src/string/IsEmpty.java
@@ -0,0 +1,10 @@
+package string;
+
+public class IsEmpty {
+    static String str = "";
+    static boolean result = str.isEmpty(); // Returns true
+
+    public static void main(String[] args) {
+        System.out.println(result);
+    }
+}
