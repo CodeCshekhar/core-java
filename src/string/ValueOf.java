aa752323fde529f0e49c3078fb94250db54da634
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-19 18:56:16 +0530

    commits

---
+++ src/string/ValueOf.java
@@ -0,0 +1,10 @@
+package string;
+
+public class ValueOf {
+    static int num = 123;
+    static String str = String.valueOf(num); // Returns "123"
+
+    public static void main(String[] args) {
+        System.out.println(str);
+    }
+}
