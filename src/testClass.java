22c9116c936b6d411e2d669717b287b3b0f5afe7
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-19 18:56:06 +0530

    commits

---
+++ src/testClass.java
@@ -0,0 +1,92 @@
+import java.util.LinkedList;
+
+
+
+
+
+public class testClass {
+    public static void main(String[] args) {
+     /*   int a =  6;
+        int b= 2;
+
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a+b);
+        System.out.println(a%b);*/
+//*****************************************************************
+
+/*
+        String s = "Hackerearth!";
+        String t = s.substring(6,12) + s.substring(12,6);
+        System.out.println(t);
+
+
+        hack_thread h = new hack_thread();
+        h.start();
+        while(h.isAlive()){
+            try{
+                h.sleep(10);
+            }catch(Exception e){
+                System.out.println("hello");
+            }
+            System.out.println("HackerEarth");
+        }
+        }
+    }
+
+    class hack_thread extends Thread{
+        @Override
+        public void run() {
+            System.out.println("hi");
+*/
+
+//****************************************************************************************8
+
+    /*for (int i =0;i<10;i++){
+        if (i==1)
+            break;
+        else {
+            System.out.println(i);
+            continue;}}*/
+        //*************************************************************************
+
+//        LinkedList<String> list = new LinkedList<String>();
+//        list.add("Allce");
+//        list.add("bob");
+//        list.add(3,"mike");
+//        list.add(3);
+//****************************************************************************************
+
+       /* testClass testClass = new testClass();
+        testClass.print(10);
+        }
+    {
+        public void print(Integer i){
+        System.out.println("interger");}
+
+        public void print(int i){
+        System.out.println("int");}
+
+        public void print(long i){
+        System.out.println("long");
+
+    }*/
+ //**********************************************************************************************
+    /*    StringBuilder s1 = new StringBuilder("1");
+        StringBuilder s2 =new StringBuilder(1);
+        String s3 = s2.substring(0);
+        char s4 = s1.charAt(0);*/
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
