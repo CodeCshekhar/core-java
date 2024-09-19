c5ecd280c84916a0c710fd867b7926555a151467
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-19 18:55:40 +0530

    commits

---
+++ src/string/LastIndexOf.java
@@ -0,0 +1,10 @@
+package string;
+
+public class LastIndexOf {
+    static String str = "Hello World World";
+    static int index = str.lastIndexOf("World"); // Returns 12
+
+    public static void main(String[] args) {
+        System.out.println(index);
+    }
+}
