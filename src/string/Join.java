1447ccb8a89d2b278dbfa1c6cf9a46d09de584b4
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-19 18:55:18 +0530

    commits

---
+++ src/string/Join.java
@@ -0,0 +1,9 @@
+package string;
+
+public class Join {
+    static String joined = String.join(", ", "Apple", "Banana", "Cherry"); // Returns "Apple, Banana, Cherry"
+
+    public static void main(String[] args) {
+        System.out.println(joined);
+    }
+}
