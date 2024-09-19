841a72675c439230da725dd0be2d596be3c135f0
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-19 18:56:01 +0530

    commits

---
+++ src/string/SubString.java
@@ -0,0 +1,10 @@
+package string;
+
+public class SubString {
+    static String str = "Hello World";
+    static String sub = str.substring(6); // Returns "World"
+
+    public static void main(String[] args) {
+        System.out.println(sub);
+    }
+}
